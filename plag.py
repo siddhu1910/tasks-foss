211994e827eecf390fe76bf10de951e2a28659e9
Mustaq Ahammad <43813438+mustaqmustu@users.noreply.github.com>
2018-10-25 19:41:50 +0530

    code to search the key words in given file  in google by googler function

---
+++ plag.py
@@ -0,0 +1,13 @@
+#code to read and search the key words from given file in google from CLI
+a=input("file name: ")
+#enter the name of file present in your directory
+b=open(a,"r")
+c=b.read()
+d=c.split()
+print(d)
+#the key words got printed on your terminal
+#now install googler from CLI by commands 1)sudo add-apt-repository ppa:twodopeshaggy/jarun and 2)sudo apt-get update && sudo apt-get install googler
+#type googler on your terminal and press enter
+#now enter your desired words from given text file in double quotes and press enter
+#you can see the top results with index numbers...press the requiured index number to go the required page
+#this may helps to check whether given file is copied from any websites 
