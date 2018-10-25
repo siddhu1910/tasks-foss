6982ae7027cf5d2ccf0abe1754c3365e4ad51652
Mustaq Ahammad <43813438+mustaqmustu@users.noreply.github.com>
2018-10-25 19:44:21 +0530

    code to update your twitter status from command line interface

--- tweet.py
+++ tweet.py
@@ -1,9 +1,9 @@
 # code to update your twitter status from command line interface
 import tweepy
-consumer_key='XKiwkkilYyD8p6B8sxZvmNuW4'
-consumer_secret='ePOyEbRnmZxQIDa7d0le2QSSNScIh6GBLhvKrPiWAnB200HQyz'
-access_token='1052785060598796288-5JsYYLvlTRYCk4hS8jTpVBQPjLtzZx'
-access_token_secret='jC0cI9UAsGrHyn9lsSa0yHdh0XTfgWGV7DQkKpYyTE7y7'
+consumer_key='<your consumer key>'
+consumer_secret='<your consumer secret>'
+access_token='<your access token>'
+access_token_secret='<your acess token secret>'
 
 auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
