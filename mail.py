ee4279c32340e0316bafdce3a2f9c1511298a6f0
Mustaq Ahammad <43813438+mustaqmustu@users.noreply.github.com>
2018-10-25 19:31:33 +0530

    code to send gmail from CLI instead of using gmail api by using smtplib library

---
+++ mail.py
@@ -0,0 +1,16 @@
+#code to send emails from cli instead of using gmail api using smtplib library 
+from getpass import getpass
+import smtplib
+conn =smtplib.SMTP('smtp.gmail.com',587)
+type(conn)
+conn.ehlo()
+conn.starttls()
+d=input("your gmail id: ")
+c=getpass("password: ")
+conn.login(d,c)
+a=input("to :")
+b=input("description :")
+conn.sendmail(d,a,b)
+conn.quit()
+()
+print("email successfull sent")
