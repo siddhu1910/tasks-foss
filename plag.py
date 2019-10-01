a819d92ae642105407d66fc87863d4c9d985c0d8
siddhu1910 <55806191+siddhu1910@users.noreply.github.com>
2019-10-01 21:39:38 +0530

    updated plag.py

--- plag.py
+++ plag.py
@@ -1,7 +1,7 @@
 #code to read and search the key words from given file in google from CLI
-a=input("file name: ")
+file=input("enter file name: ")
 #enter the name of file present in your directory
-b=open(a,"r")
+b=open(file,"r")
 c=b.read()
 d=c.split()
 print(d)
