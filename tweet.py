ef5fdafdb165b8018664cec92ba11a918408bd9a
siddhu1910 <55806191+siddhu1910@users.noreply.github.com>
2019-10-01 21:45:25 +0530

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,9 +1,9 @@
 # code to update your twitter status from command line interface
 import tweepy
 consumer_key='<your consumer key>'
-consumer_secret='<your consumer secret>'
+consumer_secret='<your consumer private>'
 access_token='<your access token>'
-access_token_secret='<your acess token secret>'
+access_token_secret='<your acess token private>'
 
 auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
